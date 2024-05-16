a947225809488b90b79cc05bb6bf1ab914d75066
“smlc” <7106775+smlc@users.noreply.github.com>
2024-05-16 09:24:49 +0100

    Blocking queue with Reentrantlock

---
+++ README.md
@@ -0,0 +1,37 @@
+# Virtual thread playground.
+
+This is a repo I use to play with Java virtual thread to understand their limitation and advantage.
+I will implement some classical multithreading problems using virtual thread and normal thread.
+
+
+## Blocking queue
+
+To implement a blocking queue we mainly need to block the different thread to not access the queue at the same time. 
+So this is mainly a synchronization problem.
+
+The different way to do synchronization of threads in Java are :
+
+- `synchronized`
+- `ReentrantLock`
+- `ReadWriteLock`
+- `Atomic Classes`
+- `Semaphore`
+
+### Synchronized and virtual thread
+
+Synchronization made using the `synchronized` keyword can block (pinned) a virtual thread. 
+That why if we are using virtual thread we should avoid anything implement using this kind of Synchronization.
+
+Why `synchronized` pinned virtual thread and not ReentrantLock ?
+When virtual thread enter a synchronized block the carrier thread will get blocked, it's not the case for ReentrantLock,
+because they are not implemented with synchronized.
+
+What does it mean the lock are reentrant ?
+
+Reentrancy means a thread can acquire the same lock it already holds. This is useful when a synchronized method calls another synchronized method of the same object. 
+Without reentrancy, such calls would deadlock.
+
+ReentrantLock resources: 
+- https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/locks/ReentrantLock.html
+- https://stackoverflow.com/a/11821900/7672189
+- https://web.mit.edu/6.005/www/fa15/classes/23-locks/
--- src/com/mars/learn/blockingqueue/BlockingQueue.java
+++ src/com/mars/learn/blockingqueue/BlockingQueue.java
@@ -1,5 +1,9 @@
 package com.mars.learn.blockingqueue;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BlockingQueue<T> {
 
     T[] array;
@@ -8,11 +12,71 @@ public class BlockingQueue<T> {
     int head = 0;
     int tail = 0;
 
+    Lock reentrantLock = new ReentrantLock();
+    private final Condition queueFull = reentrantLock.newCondition();
+    private final Condition queueEmpty = reentrantLock.newCondition();
     private final Object lock = new Object();
 
     public BlockingQueue(int capacity) {
         array = (T[]) new Object[capacity];
         this.capacity = capacity;
+    }
+
+    public void enqueueReentrantLock(T item) throws InterruptedException {
+        // How the synchronized (lock)  should be replace ? Use lock() function
+        reentrantLock.lock();
+        try {
+
+            while(size == capacity) {
+                //Queue is full wait before enqueue new item, how do we wait using reentrantLock ?
+                // We need a condition variable from the reentrantLock
+                queueFull.await();
+            }
+
+            if(tail == capacity) {
+                tail = 0;
+            }
+            //Enqueue item
+            array[tail] = item;
+            tail++;
+            size++;
+
+            //How do I notify other block thread ?
+            // Use signalAll to notify that the queue is not empty anymore.
+            queueEmpty.signalAll();
+        } finally {
+            //unlock in any case
+            reentrantLock.unlock();
+        }
+    }
+
+    public T dequeueReentrantLock() throws InterruptedException {
+
+        reentrantLock.lock();
+        T returnItem = null;
+        try {
+
+            while(size == 0) {
+                //The queue is empty let's wait signal, that a new item was added
+                queueEmpty.await();
+            }
+
+            if(head == capacity) {
+                head = 0;
+            }
+
+            returnItem = array[head];
+            array[head] = null;
+            head++;
+            size--;
+
+            //Notify thread waiting for the queue not being full anymore.
+            queueFull.signalAll();
+            return returnItem;
+        } finally {
+            //unlock in any case
+            reentrantLock.unlock();
+        }
     }
 
     public void enqueue(T item) throws InterruptedException {
@@ -62,7 +126,7 @@ public class BlockingQueue<T> {
         Thread enqueue = Thread.ofVirtual().unstarted(() -> {
             try {
                 for (int i = 0; i < 50; i++) {
-                    q.enqueue(i);
+                    q.enqueueReentrantLock(i);
                     System.out.println("enqueued " + i);
                 }
             } catch (InterruptedException e) {
@@ -73,7 +137,7 @@ public class BlockingQueue<T> {
         Thread firstDequeued = Thread.ofVirtual().unstarted(() -> {
             try {
                 for (int i = 0; i < 25; i++) {
-                    System.out.println("Thread 2 dequeued: " + q.dequeue());
+                    System.out.println("Thread 2 dequeued: " + q.dequeueReentrantLock());
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -83,7 +147,7 @@ public class BlockingQueue<T> {
         Thread secondDequeued = Thread.ofVirtual().unstarted(() -> {
             try {
                 for (int i = 0; i < 25; i++) {
-                    System.out.println("Thread 3 dequeued: " + q.dequeue());
+                    System.out.println("Thread 3 dequeued: " + q.dequeueReentrantLock());
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
