2dbc1bac02ef24b33ccb27777c5b7a3aa979b22b
“smlc” <7106775+smlc@users.noreply.github.com>
2024-08-24 14:52:47 +0100

    Update DeferredCallback.java

--- src/com/mars/learn/deferredcallback/DeferredCallback.java
+++ src/com/mars/learn/deferredcallback/DeferredCallback.java
@@ -2,6 +2,11 @@ package com.mars.learn.deferredcallback;
 
 import java.time.Duration;
 import java.util.PriorityQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -14,21 +19,39 @@ public class DeferredCallback {
 
     // Condition to make execution thread wait on
     Condition newCallbackArrived = lock.newCondition();
-
+    Future<?> future;
+    ScheduledExecutorService scheduler;
     // Run by the Executor Thread
-    public void start() throws InterruptedException {
-        while (true) {
-            lock.lock();
-        }
+    public void start() throws InterruptedException, ExecutionException {
+        scheduler = Executors.newSingleThreadScheduledExecutor();
+        if(queue.isEmpty()) return;
+
+        Callback callback = queue.peek();
+        future = scheduler.schedule(() -> {
+
+            callback.execute();
+            queue.remove(callback);
+        }, callback.executeAt, TimeUnit.SECONDS);
+
+        future.get(); // Wait for the scheduled task to finish
+        scheduler.shutdown();
+
     }
 
     public void registerCallback(Callback callback) throws InterruptedException {
-
+        //Do I need to sync this method ?
         // How to add mutex around the add method ?
         // We will use rentrant lock as mutex
         lock.lock();
+        if(!queue.isEmpty() && (queue.peek().executeAt >  callback.executeAt)) {
+            queue.add(callback);
+        } else if (!queue.isEmpty() && (queue.peek().executeAt < callback.executeAt)) {
+
+        }
         queue.add(callback);
-        newCallbackArrived.signal();
+
+
+        newCallbackArrived.signalAll();
         lock.unlock();
 
     }
